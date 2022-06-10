c9c7a9a244085c378d7d4e2945c87872d246948d
Gabriel Martinez Bidal <GMartin4@scgcontractor.com>
2022-06-09 23:58:08 -0300

    first working version of solver.py

--- entrega_2/solver.py
+++ entrega_2/solver.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import time
+
 import networkx as nx
-import matplotlib.pyplot as plt
+from graph import Graph
+from color_pallet import ColorPallet
 
 
 def solve_it(input_data):
@@ -20,19 +23,37 @@ def solve_it(input_data):
         parts = line.split()
         edges.append((int(parts[0]), int(parts[1])))
 
-    graph = nx.Graph()
-    nodes = []
-    graph.add_nodes_from([*range(node_count)])
-    graph.add_edges_from(edges)
+    color_pallet = ColorPallet()
+    amount_of_colors = 4
 
-    nx.draw(graph)
-    plt.show()
-    nx.draw_random(graph)
-    plt.show()
-    nx.draw_circular(graph)
-    plt.show()
-    nx.draw_spectral(graph)
-    plt.show()
+    graph = Graph()
+    graph.graph.add_nodes_from([id_node for id_node in range(node_count)], color=-1, assignation_number=-1, possible_color=[*range(amount_of_colors)])
+    graph.graph.add_edges_from(edges)
+
+    amount_assign = 0
+    nodes_resolved = set()
+    node = graph.get_most_connected_node()
+    while node is not None:
+        color_of_surrounding_nodes = graph.get_colors_of_surrounding_nodes(node)
+        color = color_pallet.get_color(color_excluding=color_of_surrounding_nodes)
+        resolved = graph.set_color_of_node(node, color, amount_assign)
+        if not resolved:
+            graph.plot(flag_current=True)
+            time.sleep(1.5)
+            print('', end='')
+            print("Unable to propagate")
+
+        nodes_resolved.add(node)
+        node = graph.get_node_with_least_possibilities(exclude=nodes_resolved)
+        amount_assign += 1
+
+        if amount_assign < 0:
+            graph.plot()
+            time.sleep(1.5)
+            print('', end='')
+
+    graph.plot()
+
 
     # build a trivial solution
     # every node has its own color
@@ -50,7 +71,7 @@ import sys
 if __name__ == '__main__':
     import sys
     if len(sys.argv) <= 1:
-        case_number = input('Give the case number:')
+        case_number = '20_9'  # input('Give the case number:')
         file_location = 'data/gc_' + case_number
     if len(sys.argv) > 1:
         file_location = sys.argv[1].strip()
