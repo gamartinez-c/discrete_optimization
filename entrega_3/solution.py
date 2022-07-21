5aa7d9e137408a11439e185b1326926221451341
Gabriel Martinez Bidal <GMartin4@scgcontractor.com>
2022-07-21 18:21:51 -0300

    Add neighbours piece.

--- entrega_3/solution.py
+++ entrega_3/solution.py
@@ -1,4 +1,5 @@
 import random
+import logging
 
 from route import Route
 from location import Location
@@ -34,8 +35,93 @@ class Solution:
         for location in location_list:
             self.route.add_location(location)
 
-    def improve_solution_with_neighbour(self, location_to_improve):
-        pass
+    def change_location_position(self, location_to_improve):
+        initial_index = self.route.sequence_list.index(location_to_improve)
+
+        best_position = initial_index
+        best_obj_value = self.get_obj_value()
+
+        for i in range(len(self.route)):
+            self.route.remove_location(location_to_improve)
+            self.route.add_location(location_to_improve, index=i)
+            obj_value = self.get_obj_value()
+            if obj_value < best_obj_value:
+                best_position = i
+                best_obj_value = obj_value
+
+        # Set to best solution
+        self.route.remove_location(location_to_improve)
+        self.route.add_location(location_to_improve, index=best_position)
+
+    def improve_looking_for_neighbours(self, loops_for_swaps=300, loops_for_breaking_bad_connections=300):
+        initial_value = self.get_obj_value()
+
+        prev_location_moved = None
+        location_to_move = self.route.get_loc_with_most_travel_times()
+
+        # self.plot()
+        # print(self.get_obj_value())
+        i = 0
+        exclude_locations = []
+        while prev_location_moved != location_to_move and i < loops_for_swaps and i <= len(Location.locations_list) - 2:
+            original_sequence = self.route.sequence_list.copy()
+            original_obj_value = self.get_obj_value()
+
+            self.change_location_position(location_to_move)
+            exclude_locations.append(location_to_move)
+
+            if self.get_obj_value() > original_obj_value:
+                self.route.reset_route_with(original_sequence)
+
+            prev_location_moved = location_to_move
+            location_to_move = self.route.get_loc_with_most_travel_times(exclude_locations=exclude_locations)
+            i += 1
+        j = i
+        # self.plot()
+        # print(self.get_obj_value())
+
+        i = 0
+        prev_worst_connection = None
+        worst_connection_tuple = self.route.get_locs_of_worst_connection()
+        while prev_worst_connection != worst_connection_tuple and i < loops_for_breaking_bad_connections:
+            self.improve_just_1_link(worst_connection_tuple[0], worst_connection_tuple[1])
+
+            prev_worst_connection = worst_connection_tuple
+            worst_connection_tuple = self.route.get_locs_of_worst_connection(exclude_list=[worst_connection_tuple])
+            i += 1
+        # self.plot()
+        final_value = self.get_obj_value()
+
+        logging.info("neighbout 1: " + str(j) + " neighbout 2: " + str(i) + " Improvement: " + str(initial_value - final_value))
+
+    def improve_just_1_link(self, src_loc, dest_loc):
+        original_sequence = self.route.sequence_list.copy()
+        original_obj_value = self.get_obj_value()
+
+        loc_to_exclude_list = [src_loc, self.route.get_sequence_location(self.route.sequence_list.index(dest_loc)+1)]
+        ideal_loc_for_dest = dest_loc.get_nearest_location(exclude_location_list=loc_to_exclude_list)
+        index_of_ideal_loc_for_dest = self.route.sequence_list.index(ideal_loc_for_dest)
+        index_of_src = self.route.sequence_list.index(src_loc)
+        index_of_dest = self.route.sequence_list.index(dest_loc)
+
+        index_to_start_extraction = (index_of_dest + 1) % len(self.route)
+        index_of_end_extraction = index_of_ideal_loc_for_dest
+
+        if index_to_start_extraction < index_of_end_extraction:
+            sequence_to_extract = self.route.sequence_list[index_to_start_extraction: index_of_ideal_loc_for_dest]
+        else:
+            sequence_to_extract = []
+            sequence_to_extract += self.route.sequence_list[index_to_start_extraction:]
+            sequence_to_extract += self.route.sequence_list[:index_to_start_extraction]
+
+        for location in sequence_to_extract:
+            self.route.remove_location(location)
+        # By how we are inserting the sequence is reverse that is what we need
+        for location in sequence_to_extract:
+            self.route.add_location(location, index_of_src + 1)
+
+        if self.get_obj_value() > original_obj_value:
+            self.route.reset_route_with(original_sequence)
 
     def get_obj_value(self):
         return self.route.get_total_distance_travel()
